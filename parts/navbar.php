7738e17ed23a3c1b30050ec50c448455da17c759
labybismail <labybismail.dev@gmail.com>
2024-12-18 20:50:26 +0100

    design fixes

--- parts/navbar.php
+++ parts/navbar.php
@@ -1,42 +1,47 @@
-
 <style>
-        .navbar {
-            position: fixed;
-            top: 0;
-            left: 4px;
-            width: fit-content;
-            z-index: 1;
-        }
-        .navbar a {
-            width: 40px;
-            height: 40px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            background-color: #f8f9fa;
-            border-radius: 8px;
-            margin-right: 8px;
-            text-decoration: none;
-        }
+    .navbar {
+        position: fixed;
+        top: 0;
+        left: 4px;
+        /* width: fit-content; */
+        z-index: 1;
+    }
 
-        .navbar a i {
-            color: #111213;
-        }
+    .navbar a {
+        width: 40px;
+        height: 40px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #f8f9fa;
+        border-radius: 8px;
+        margin-right: 8px;
+        text-decoration: none;
+    }
 
-        .navbar a:hover {
-            background-color:rgb(88, 86, 85);
-        }
-    </style>
+    .navbar a i {
+        color: #111213;
+    }
+
+    .navbar a:hover {
+        background-color: rgb(88, 86, 85);
+    }
+</style>
 
 <nav class="navbar navbar-dark bg-none">
-        <div class="row">
-            <div class="col-12 d-flex">
-                <a href="index.php">
-                    <i class="fa-solid fa-house-chimney"></i>
-                </a>
-                <a href="uploadExcel.php">
-                    <i class="fa-solid fa-file-excel"></i>
-                </a>
-            </div>
+    <div class="row">
+        <div class="col-12 d-flex">
+            <a href="index.php">
+                <i class="fa-solid fa-house-chimney"></i>
+            </a>
+            <a href="uploadExcel.php">
+                <i class="fa-solid fa-file-excel"></i>
+            </a>
+        <?php// if(strtolower(pathinfo(__FILE__, PATHINFO_FILENAME))=='uploadexcel'){?>
+            <a href="db/migration.php" onclick="if(!confirm('This action will reset the database !!! \n Are you sure you want to refresh database from last updated excel file ?')){return false;}" class="bg-warning">
+                <i class="fa-solid fa-database" style="color: #0c0d0d;"></i>
+            </a>
+        <?php// }?>
         </div>
-    </nav>
+    </div>
+</nav>
--- uploadExcel.php
+++ uploadExcel.php
@@ -7,11 +7,15 @@
     <title>Upload Excel</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <link rel="stylesheet" href="css/ExcelUpload.css">
 </head>
+<body
+    style="  background-image: url('images/Searchs_005.jpg');background-repeat:no-repeat;background-size:cover;">
 
-<body>
 <?php require 'parts/navbar.php';?>
 
     <form action="op/maj.php" method="POST" enctype="multipart/form-data">
